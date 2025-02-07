ac1e315773aea4c8fc7dff97ec7a58caa0698b39
marisraw <marislyf@gmail.com>
2025-02-07 12:14:31 +0530

    Updated

--- SQL PROJECT WORK - HomeToHome.sql
+++ SQL PROJECT WORK - HomeToHome.sql
@@ -8,6 +8,9 @@ USE ShopTOShop;
 -- END
 
 
+-- * * * * * * * * * * * * * * * * * * --
+
+
 -- Creating a table for Customers
 CREATE TABLE Customers
 (
@@ -18,9 +21,23 @@ Place VARCHAR(50) NOT NULL
 );
 -- END
 
+
+-- Inserting data in Customers table
+INSERT INTO Customers 
+VALUES
+(1, "Arun", "9785265455", "Chennai"),
+(2, "Praksh", "5896544555", "Vellore"),
+(3, "Ananthi", "8965432168", "Thirunelveli"),
+(4, "Selvam", "7856315458", "kanchipuram"),
+(5, "Kumar", "7835642136", "Trichy");
+-- END
+
+
 -- Select a Customers table
 Select * From Customers;
 -- END
+
+-- * * * * * * * * * * * * * * * * * * --
 
 
 -- Creating a table for Merchants
@@ -33,9 +50,24 @@ Place VARCHAR(50) NOT NULL
 );
 -- END
 
+
+-- Inserting data in Merchants table
+INSERT INTO Merchants 
+VALUES
+(1, "Anitha", "354644865", "Tenkasi"),
+(2, "Vinoth", "79956644", "Nelloor"),
+(3, "Sam", "856254587", "Ooty"),
+(4, "Aravind", "98785665", "Avinasi"),
+(5, "Suguna", "3654854887", "Madurai");
+-- END
+
+
 -- Select a merchants table
 Select * From Merchants;
 -- END 
+
+
+-- * * * * * * * * * * * * * * * * * * --
 
 
 -- Creating a table for orders
@@ -49,9 +81,34 @@ CONSTRAINT FK_Orders_Customer_ID FOREIGN KEY (Customer_ID) REFERENCES Customers 
 );
 -- END
 
+
+-- Inserting data in Orders table
+INSERT INTO Orders 
+VALUES
+(1, "Mango", 500, 2),
+(2, "Brinjal", 250, 2),
+(3, "Carrot", 100, 4),
+(4, "Beetroot", 725, 3),
+(5, "Tomatos", 900, 1);
+-- END
+
+
+-- Add "ON DELETE SET NULL" PROCESS FOR Orders Table
+ALTER TABLE Orders
+DROP CONSTRAINT FK_Orders_Customer_ID;
+
+ALTER TABLE Orders
+ADD CONSTRAINT FK_Orders_Customer_ID FOREIGN KEY (Customer_ID) REFERENCES Customers (Customer_ID)
+ON DELETE SET NULL;
+-- END
+
+
 -- Select a orders table
 Select * From orders;
 -- END 
+
+
+-- * * * * * * * * * * * * * * * * * * --
 
 
 -- Creating a table for Payments
@@ -65,9 +122,34 @@ CONSTRAINT FK_Payments_Order_ID FOREIGN KEY (Order_ID) REFERENCES Orders (Order_
 );
 -- END
 
+
+-- Inserting data in Payments table
+INSERT INTO Payments 
+VALUES
+(1, "UPI", "Successfull", 2),
+(2, "UPI", "Successfull", 2),
+(3, "Card", "Failure", 4),
+(4, "Card", "Failure", 3),
+(5, "UPI", "Successfull", 5);
+-- END
+
+
+-- Add "ON DELETE SET NULL" PROCESS FOR Payments Table
+ALTER TABLE Orders
+DROP CONSTRAINT FK_Payments_Order_ID;
+
+ALTER TABLE Payments
+ADD CONSTRAINT FK_Payments_Order_ID FOREIGN KEY (Order_ID) REFERENCES Orders (Order_ID)
+ON DELETE SET NULL;
+-- END
+
+
 -- Select a payments table
 Select * From payments;
 -- END 
+
+
+-- * * * * * * * * * * * * * * * * * * --
 
 
 -- Creating a table for Shippings
@@ -84,59 +166,6 @@ CONSTRAINT FK_Shippings_Order_ID FOREIGN KEY (Order_ID) REFERENCES Orders (Order
 );
 -- END
 
--- Select a shippings table
-Select * From shippings;
--- END 
-
-
--- Show all the table where created
-SHOW TABLES;
--- END
-
-
--- Inserting data in Customers table
-INSERT INTO Customers 
-VALUES
-(1, "Arun", "9785265455", "Chennai"),
-(2, "Praksh", "5896544555", "Vellore"),
-(3, "Ananthi", "8965432168", "Thirunelveli"),
-(4, "Selvam", "7856315458", "kanchipuram"),
-(5, "Kumar", "7835642136", "Trichy");
--- END
-
-
--- Inserting data in Orders table
-INSERT INTO Orders 
-VALUES
-(1, "Mango", 500, 2),
-(2, "Brinjal", 250, 2),
-(3, "Carrot", 100, 4),
-(4, "Beetroot", 725, 3),
-(5, "Tomatos", 900, 1);
--- END
-
-
--- Inserting data in Merchants table
-INSERT INTO Merchants 
-VALUES
-(1, "Anitha", "354644865", "Tenkasi"),
-(2, "Vinoth", "79956644", "Nelloor"),
-(3, "Sam", "856254587", "Ooty"),
-(4, "Aravind", "98785665", "Avinasi"),
-(5, "Suguna", "3654854887", "Madurai");
--- END
-
-
--- Inserting data in Payments table
-INSERT INTO Payments 
-VALUES
-(1, "UPI", "Successfull", 2),
-(2, "UPI", "Successfull", 2),
-(3, "Card", "Failure", 4),
-(4, "Card", "Failure", 3),
-(5, "UPI", "Successfull", 5);
--- END
-
 
 -- -- Inserting data in Shippings table
 INSERT INTO Shippings 
@@ -148,119 +177,211 @@ VALUES
 (5, "Pending", 2, 4, 3);
 -- END
 
--- !!! QUERIES !!! --
 
--- 1 .  Who ordered what quantity?
+-- Add "ON DELETE SET NULL" PROCESS FOR Shippings Table
+ALTER TABLE Shippings
+DROP CONSTRAINT FK_Shippings_Customer_ID;
+
+ALTER TABLE Shippings
+DROP CONSTRAINT FK_Shippings_Merch_ID;
+
+ALTER TABLE Shippings
+DROP CONSTRAINT FK_Shippings_Order_ID;
+
+ALTER TABLE Shippings
+ADD CONSTRAINT FK_Shippings_Customer_ID FOREIGN KEY (Customer_ID)
+REFERENCES Customers (Customer_ID)
+ON DELETE SET NULL;
+
+ALTER TABLE Shippings
+ADD CONSTRAINT FK_Shippings_Merch_ID FOREIGN KEY (Merch_ID)
+REFERENCES Merchants (Merch_ID)
+ON DELETE SET NULL;
+
+ALTER TABLE Shippings
+ADD CONSTRAINT FK_Shippings_Order_ID FOREIGN KEY (Order_ID)
+REFERENCES Orders (Order_ID)
+ON DELETE SET NULL;
+-- END
+
+
+-- Select a shippings table
+Select * From shippings;
+-- END 
+
+-- * * * * * * * * * * * * * * * * * * --
+
+
+-- Show all the table where created
+SHOW TABLES;
+-- END
+
+
+-- * * * * * * * * * * * * * * * * * * --
+
+
+-- !!! STORED PROCEDURES !!! --
+
+-- 1 .  Show all customers ordered  quantity?
+
+create view Show_all_customers_ordered_quantity
+AS
 SELECT customer_id, COUNT(order_id) AS Order_Counts
 FROM orders
 GROUP BY orders.customer_id;
+
+select * from Show_all_customers_ordered_quantity;
+
+
+
+
 -- END
 
 
--- 2 .  Who order more than one time
+-- 2 .  Who ordered by  more than one time or any?
+
+Delimiter $$
+create procedure Who_Orderd_By_More_than (in input_Order_Count int)
+begin
 SELECT customer_id, COUNT(order_id) AS Order_Counts
 FROM orders
 GROUP BY orders.customer_id
-HAVING COUNT(orders.order_id) > 1;
+HAVING COUNT(orders.order_id) > input_Order_Count;
+end $$
+Delimiter ;
+
+call Who_Orderd_By_More_than (1);
+
 -- END
 
 
--- 3 .  Who ordered two times
+-- 3 .  Who ordered How much times
+
+Delimiter $$
+create procedure Who_Orderd_How_Much_time (in input_Order_Count int)
+begin
 SELECT customer_id, COUNT(order_id) AS Order_Counts
 FROM orders
 GROUP BY orders.customer_id
-HAVING COUNT(orders.order_id) = 2;
+HAVING COUNT(orders.order_id) = input_Order_Count;
+end $$
+Delimiter ;
+
+call Who_Orderd_How_Much_time  (1);
+
 -- END
 
 
--- 4. 2nd Customer delivery status
+-- 4. Check any Customer's order delivery status
+Delimiter $$
+create procedure Customer_order_delivery_status  (in input_CUstomer_ID int)
+begin
 SELECT customer_id, (status) AS Delivery_status
 FROM shippings
-WHERE customer_id = 2; 
+WHERE customer_id = input_CUstomer_ID; 
+end $$
+Delimiter ;
+
+call Customer_order_delivery_status  (3);
 -- END
 
 
--- 5. How many orders in pending status
+-- 5. How many orders are not deliverd
+
+create view Show_Count_of_orders_Not_deliverd
+AS
 SELECT COUNT(*) AS Pending_Orders
 FROM Shippings
 WHERE status = 'Pending';
+
+
+select * from Show_Count_of_orders_Not_deliverd;
 -- END
 
 
--- 6. How many orders are succesfully delivered
-SELECT COUNT(*) AS Pending_Orders
-FROM Shippings
-WHERE status = 'Deliverd';
--- END
+-- 6. Get list of pending counts by customers 
 
-
--- 7. Get list of pending counts by customers 
+create view Order_Pending_Counts_BY_Customers
+AS
 SELECT Customer_id, COUNT(*) AS Pending_Counts
 FROM Shippings
 WHERE status = 'Pending'
 GROUP BY Customer_id;
 
+select * from  Order_Pending_Counts_BY_Customers;
 
--- 8. Get list of deliverd counts by customers 
+
+-- 7. Get list of deliverd counts by customers 
+
+create view Deliverd_Counts_BY_Customers 
+AS
 SELECT Customer_id, COUNT(*) AS Deliverd_Counts
 FROM Shippings
 WHERE status = 'Deliverd'
 GROUP BY Customer_id;
 
-
--- 9. ///// UNCOVERED ///// Show the pending & deliverd counts by customers 
-SELECT 
-    Customer_id, 
-    COUNT(CASE WHEN status = 'Pending' THEN 1 END) AS Pending_Orders, 
-    COUNT(CASE WHEN status = 'Deliverd' THEN 1 END) AS Delivered_Orders
-FROM Shippings
-GROUP BY Customer_id;
--- END
+select * from  Deliverd_Counts_BY_Customers;
 
 
--- 10 . Pending  status of items.
+-- 8 . Which items are pending
+
+create view Which_items_are_pending
+AS
 SELECT orders.item, shippings.status
 FROM orders
 JOIN Shippings
 ON Orders.order_id = Shippings.order_id
 WHERE Shippings.status = 'Pending'
 GROUP BY Shippings.order_ID;
+
+select * from Which_items_are_pending;
+
 -- END
 
 
--- 11 . total amount of all pending orders 
+-- 09 . total amount of all pending orders
+
+create view total_amount_of_all_pending_orders 
+AS
 SELECT SUM(Orders.amount) AS Total_Pending_Amount
 FROM orders
 JOIN Shippings
 ON Orders.order_id = Shippings.order_id
 WHERE Shippings.status = 'Pending';
 
+select * from total_amount_of_all_pending_orders;
 
--- 12 . Pending amount of each items
+
+-- 10 . Pending amount of each items
+
+create view Pending_amount_of_each_items
+AS
 SELECT orders.item, SUM(Orders.amount) AS Pending_Amount
 FROM orders
 JOIN Shippings
 ON Orders.order_id = Shippings.order_id
 WHERE Shippings.status = 'Pending'
 GROUP BY Shippings.order_ID;
+
+select * from Pending_amount_of_each_items;
 -- END
 
 
+-- * * * * * * * * * * * * * * * * * * --
 
 
 -- Scenarios of this table:-
 -- 1 .  Who ordered what quantity?
 -- 2 .  Who order more than one time
 -- 3 .  Who ordered two times
--- 4. 2nd Customer delivery status
--- 5. How many orders in pending status
--- 6. How many orders are succesfully delivered
--- 7. Get list of pending counts by customers 
--- 8. Get list of deliverd counts by customers 
--- 9. ///// UNCOVERED ///// Show the pending & deliverd counts by customers 
--- 10 . Pending  status of items.
--- 11 . total amount of all pending orders 
--- 12 . Pending amount of each items
+-- 4. 2nd Customer order delivery status
+-- 5. How many orders are not deliverd
+-- 6. Get list of pending counts by customers 
+-- 7. Get list of deliverd counts by customers 
+-- 8 . Which items are pending
+-- 09 . total amount of all pending orders 
+-- 10 . Pending amount of each items
+
 
 
 
